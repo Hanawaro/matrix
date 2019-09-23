a107af44774877b426a9f3f7c1027bfbc09552ff
Hanawaro <hanawaro3@gmail.com>
2019-09-23 20:25:22 +0300

    v1 of opredelitel

--- main.c
+++ main.c
@@ -9,21 +9,33 @@
 #define C_RANG "rang"
 #define C_EXIT "shutdown"
 
-void getCommand(char *string);
+#define FC_EXIT "exit"
+
+int getCommand(char *string);
+int checkString(char *string, int *number);
 void helpShow(void);
-void opredelitelStart(void);
-void powerStart(void);
-void rangStart(void);
+int opredelitelStart(void);
+int powerStart(void);
+int rangStart(void);
+
+int getOpredelitelLine(char * line);
 
 int main(void) {
-    int status = 1;
+    int status = 1,
+        space = 0;
     char *command = (char *) malloc(sizeof(char));
-    printf("Приветствую вас в matrix\n");
+    fprintf(stdout, " Приветствую вас в matrix\n");
     do {
-        printf("~/ ");
-        getCommand(command);
+        fprintf(stdout, "~/ ");
+        space = 0;
+        int c;
+        if ( getCommand(command) )
+            space = 1;
 
-        if (!strcmp(command, C_EXIT))
+        if ( space );
+        else if ( strlen(command) == 0 )
+            fprintf(stdout, "\n");
+        else if (!strcmp(command, C_EXIT))
             status = 0;
         else if (!strcmp(command, C_HELP))
             helpShow();
@@ -34,34 +46,114 @@ int main(void) {
         else if (!strcmp(command, C_RANG))
             rangStart();
         else
-            printf("Неизвестная комманда\nВведите help, чтобы увидеть список команд\n");
-
+            fprintf(stdout, " Неизвестная комманда\nВведите help, чтобы увидеть список команд\n");
     } while (status);
+
+    fprintf(stdout, " Спасибо за использование программы.\n");
+
+    free(command);
     
     return 0;
 }
 
-void getCommand(char *string) {
+int getCommand(char *string) {
     int N = 0;
     while ( (string[N] = getchar()) != EOF && string[N] != '\n' && string[N] != '\0') {
         N++;
         string = (char *) realloc(string, sizeof(char) * N);
     }
-    string[N] = '\0';
+    if (string[N] == '\n' && N == 0) {
+        string[N] = '\0';
+        return 1;
+    } else {
+        string[N] = '\0';
+        return 0;
+    }
+}
+
+int checkString(char *string, int* number) {
+    int N = 0,
+        power = 1,
+        num = 0,
+        status = 0;
+    while (string[N] != '\0') {
+        if (string[N] >= '0' && string[N] <= '9') {
+            *number += power*((int) (string[N] - '0'));
+            power *= 10;
+            status = 1;
+        } else 
+            break;
+        N++;
+    }
+    if (status)
+        return 1;
+    else 
+        return 0;
 }
 
 void helpShow(void) {
-    printf("Список команд: \n");
-    printf(" -- %s\n", C_OPREDELITEL);
-    printf(" -- %s\n", C_POWER);
-    printf(" -- %s\n", C_RANG);
+    fprintf(stdout, " Список команд: \n");
+    fprintf(stdout, "  -- %s\n", C_OPREDELITEL);
+    fprintf(stdout, "  -- %s\n", C_POWER);
+    fprintf(stdout, "  -- %s\n", C_RANG);
+    fprintf(stdout, "  -- %s\n", C_EXIT);
 }
-void opredelitelStart(void) {
-    // Поиск определителя
+
+int opredelitelStart(void) {
+    // Заменить int на double
+    int amount = 0, status = 1;
+    char *data = (char *) malloc(sizeof(char));
+	int** matrix;
+    
+	fprintf(stdout, " Введите порядок матрицы, для нахождения её определителя: ");
+	do {
+        getCommand(data);
+        if (!strcmp(data, FC_EXIT))
+            return 0;
+        if ( checkString(data, &amount) ) {
+            if (amount <= 0)
+                printf(" Некорректные данные...\n");
+            else 
+                status = 0;
+        } else {
+            printf(" Некорректые данные...\n");
+        }
+	} while (status);
+
+	fprintf(stdout, " Введите элементы матрицы:\n");
+
+	matrix = (int**)malloc(amount * sizeof(int*));
+
+    // Сделать ввод чего угодно (нужна функция getLineOpredelitel() )
+    do {
+		status = 0;
+		for (int i = 0; i < amount; i++) {
+			matrix[i] = (int*)malloc(amount * sizeof(int));
+            printf(" ");
+			for (int j = 0; j < amount; j++)
+				if (scanf("%d", &matrix[i][j]) != 1) {
+					status = 1;
+				}
+		}
+	} while (status);
+
+	fprintf(stdout, " Определитель матрицы равен: %d\n", findOpedelitel(matrix, amount) );
+
+	for (int i = 0; i < amount; i++)
+		free(matrix[i]);
+	free(matrix);
+    free(data);
+
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF && c != '\0') { }
+
+    return 0;
 }
-void powerStart(void) {
+int powerStart(void) {
     // Поиск произведения
+    return 0;
 }
-void rangStart(void) {
+int rangStart(void) {
     // Поиск ранга
+    return 0;
 }
---
+++ matrix.h
@@ -0,0 +1,3 @@
+int findOpedelitel(int **matrix, int size);
+int findRand(int **matrix, int sizeY, int sizeX);
+int** power(int **firstMatrix,int firstSizeY, int firstSizeX, int **secondMatrix,int secondSizeY, int secondSizeX);
